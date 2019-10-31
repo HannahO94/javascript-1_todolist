9e76e0e1734cddd36180b5934e5a7ded2463d8cf
HannahO94 <56817756+HannahO94@users.noreply.github.com>
2019-10-31 18:10:24 +0100

    added som js to input and button

---
+++ main.js
@@ -0,0 +1,9 @@
+const listOfToDos = []
+const addToDobtn = document.querySelector('#addtodobtn')
+const toDoInput = document.querySelector('#')
+
+addToDobtn.addEventListener('submit', function(event){
+    event.preventDefault()
+    toDoInput
+
+})
