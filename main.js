15c6203a4249c586d222d96f8a440dc29c2efb8e
HannahO94 <56817756+HannahO94@users.noreply.github.com>
2019-11-01 08:27:57 +0100

    Lagt till saker i main och index

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const listOfToDos = []
 const addToDobtn = document.querySelector('#addtodobtn')
-const toDoInput = document.querySelector('#')
+const toDoInput = document.querySelector('#todoinput')
+const theList = document.querySelector('#theList')
 
 addToDobtn.addEventListener('submit', function(event){
     event.preventDefault()
-    toDoInput
-
+    listOfToDos.push(toDoInput)
+    creatingList(toDoInput)    
 })
+
+function creatingList(toDo){
+    const listtodo = document.querySelector('#theList')
+    const li = document.createElement('li')
+    li.textContent = toDo
+    listtodo.appendChild(li)
+        
+    }
+
+
+creatingList(toDoInput)
